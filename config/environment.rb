16596a34782f4f0f80b50856cafaf3989edc6b44
Naresh Kapse <kapse.naresh@gmail.com>
2010-10-05 17:16:55 +0530

    Updated env for rails

--- config/environment.rb
+++ config/environment.rb
@@ -17,6 +17,7 @@ Rails::Initializer.run do |config|
   # Specify gems that this application depends on and have them installed with rake gems:install
   # config.gem "bj"
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
+config.gem "rails", :lib => false, :version => '2.3.5'
 config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
 config.gem "rspec", :lib => false, :version => ">= 1.3.0"
